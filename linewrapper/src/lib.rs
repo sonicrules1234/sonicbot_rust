c50f8d15745f74500812329d1e3b99b2cb0c3af9
Westly Ward <sonicrules1234@gmail.com>
2021-07-24 11:10:39 -0600

    Git didn't seem to add the linewrapper folder.  It should be added now.

---
+++ linewrapper/src/lib.rs
@@ -0,0 +1,66 @@
+use macroquad::prelude::*;
+pub struct LineWrapper {
+    ttffont: Font,
+    lines: Vec<String>,
+    cols: u8,
+    rows: u8,
+    charh: f32,
+    charw: f32,
+    textp: TextParams,
+}
+impl LineWrapper {
+    pub fn new() -> Self {
+        let ttffont = load_ttf_font_from_bytes(include_bytes!("font.ttf")).unwrap();
+        let mut lines: Vec<String> = Vec::new();
+        let chardims = measure_text("a", Some(ttffont), 14, 1.0);
+        let charh = chardims.height + 10.0;
+        let charw = chardims.width;
+        let textp = TextParams {
+            font: ttffont,
+            font_size: 14,
+            font_scale: 1.0,
+            font_scale_aspect: 1.0,
+            color: WHITE,
+        };
+        let mut wcount = 0;
+        let mut hcount = 0;
+        let sw = screen_width();
+        let sh = screen_height();
+        while wcount as f32 * charw < sw {
+            wcount += 1;
+        }
+        wcount -= 1;
+        while hcount as f32 * charh < sh {
+            hcount += 1;
+        }
+        hcount -= 1;
+        Self {
+            ttffont: ttffont,
+            lines: lines,
+            cols: wcount,
+            rows: hcount,
+            charh: charh,
+            charw: charw,
+            textp: textp,
+        }
+    }
+    pub fn println(&mut self, data: String) -> () {
+        let output = textwrap::fill(data.as_str(), (self.cols - 2) as usize).replace("\r", "");
+        let lines = output.split("\n").collect::<Vec<&str>>();
+        for line in lines {
+            self.lines.push(line.to_string());
+            if self.lines.len() > (self.rows - 2).into() {
+                self.lines.remove(0);
+            }
+        }
+    }
+    pub fn showlines(&self) -> () {
+        clear_background(BLACK);
+        //print!("{}", &self.lines.join("\n"));
+        let mut startx = self.charh;
+        for line in &self.lines {
+            draw_text_ex(&line, self.charw, startx, self.textp);
+            startx = startx + self.charh
+        }
+    }
+}
